a731684d4a456e38f8cc54aa6fcfd7b4178247fb
zetsuboii <hmzkrb@gmail.com>
2023-07-22 20:54:31 +0300

    feat: add deployment script

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -1,15 +1,27 @@
 import { HardhatUserConfig } from "hardhat/config"
 import "@nomicfoundation/hardhat-toolbox";
 
+import dotenv from "dotenv";
+dotenv.config();
+
+const getEnv = (name: string): string => {
+  const value = process.env[name];
+  if (value == undefined) {
+    throw new Error(`Missing environment variable ${name}`);
+  }
+  return value;
+};
+
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.19",
   },
   networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545",
-      chainId: 31337,
-    },
+    sepholia: {
+      url: "https://sepolia.gateway.tenderly.co",
+      chainId: 11155111,
+      accounts: [getEnv("DEPLOYER_PRIVATE_KEY")],
+    }
   }
 };
 
