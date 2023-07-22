ba5a4f6c802466375082f99a5c2c43765f279e40
zetsuboii <hmzkrb@gmail.com>
2023-07-23 00:11:13 +0300

    feat: new deployments

--- smart-contracts/scripts/call.ts
+++ smart-contracts/scripts/call.ts
@@ -4,22 +4,31 @@ import * as fs from "fs";
 const deployments = JSON.parse(fs.readFileSync("deployments.json", "utf8"));
 const address = deployments["eventReader"];
 const addressCcip = deployments["eventReaderCcipEnabled"];
+const addressManager = deployments["eventManager"];
 
 
 async function call() {
-    const abi = [
+    const readerAbi = [
         "function getReputation(bytes32 domain, address user) view returns (uint256)"
     ];
+
+    const managerAbi = [
+        "function getAllEvents(bytes32 domain, address user) view returns (tuple(uint8 eventType, string name, bytes extraData)[])",
+    ]
 
     const domain = ethers.utils.id("Hello");
     const queryAddress = ethers.Wallet.createRandom().address;
 
-    const contract = new ethers.Contract(address, abi, ethers.provider);
-    const response = await contract.getReputation(domain, queryAddress, { enableCcipRead: true });
+    const contract = new ethers.Contract(address, readerAbi, ethers.provider);
+    const response = await contract.getReputation(domain, queryAddress, { ccipReadEnabled: true });
     console.log("Response: ", response);
 
-    const contractCcip = new ethers.Contract(addressCcip, abi, ethers.provider);
-    const responseCcip = await contractCcip.getReputation(domain, queryAddress, { enableCcipRead: true });
+    const eventManager = new ethers.Contract(addressManager, managerAbi, ethers.provider);
+    const allEvents = await eventManager.getAllEvents(domain, queryAddress);
+    console.log("All events: ", allEvents);
+    
+    const contractCcip = new ethers.Contract(addressCcip, readerAbi, ethers.provider);
+    const responseCcip = await contractCcip.getReputation(domain, queryAddress, { ccipReadEnabled: true });
     console.log("Response CCIP: ", responseCcip);
 }
 
